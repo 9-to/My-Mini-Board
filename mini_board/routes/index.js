c265d6958b82aadf4de40674e94701d45c435b2b
so9to <47969821+9-to@users.noreply.github.com>
2022-02-14 10:05:12 +0900

    完成

--- mini_board/routes/index.js
+++ mini_board/routes/index.js
@@ -1,9 +1,72 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var knex = require('knex')({
+  client: 'mysql',
+  connection:{
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'my-nodeapp-db00',
+    charset: 'utf8'
+  }
+});
+var Bookshelf = require('bookshelf')(knex);
+
+var User = Bookshelf.Model.extend({
+  tableName: 'users',
+});
+var Message = Bookshelf.Model.extend({
+  tableName: 'messages',
+  hasTimestamps: true,
+  user: function(){
+    return this.belongsTo(User);
+  },
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next)=> {
+  if(req.session.login == null){
+    res.redirect('/users/login');
+  }else{
+    res.redirect('/1');
+  }
 });
+
+router.get('/:page', (req, res, next)=> {
+  if(req.session.login == null){
+    res.redirect('/users/login');
+    return;
+  }
+  var pg = req.params.page;
+  pg *= 1;
+  if(pg<1){pg=1;}
+  new Message().orderBy('created_at','DESC')
+      .fetchPage({page:pg,pageSize:10,withRelated:['user']})
+      .then((collection)=>{
+    var data = {
+      title: 'miniBoard',
+      login: req.session.login,
+      collection: collection.toArray(),
+      pagination: collection.pagination,
+    };
+    res.render('index', data);
+  }).catch((err)=>{
+    res.status(501).json({error: true, data:{message: err.message}});
+  });
+});
+
+/*POST*/
+router.post('/',(rq,rs,next)=>{
+  var rec = {
+    message: rq.body.msg,
+    user_id: rq.session.login.id,
+  }
+  new Message(rec).save().then((model)=>{
+    rs.redirect('/');
+  });
+});
+
 
 module.exports = router;
--- mini_board/routes/users.js
+++ mini_board/routes/users.js
@@ -1,9 +1,118 @@
 var express = require('express');
 var router = express.Router();
+var {check, validationResult} = require('express-validator');
+var mysql = require('mysql');
+
+var knex = require('knex')({
+  client: 'mysql',
+  connection:{
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'my-nodeapp-db00',
+    charset: 'utf8'
+  }
+});
+var Bookshelf = require('bookshelf')(knex);
+
+var User = Bookshelf.Model.extend({
+  tableName: 'users',
+});
+
+function vCheck(){
+  return [check('name','NAMEは必須項目です').notEmpty(),
+  check('password','PASSWORDは必須項目です').notEmpty()]
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/add', (req, res, next)=> {
+  var data = {
+    title: 'Users/Add',
+    form:{name:'',password:'',comment:''},
+    content:'登録するname,pw,commentを入力ください',
+  }
+  res.render('users/add',data);
+});
+
+router.get('/login',(rq,rs,next)=>{
+  var data = {
+    title: 'Users/Login',
+    form:{name:'',password:''},
+    content:'name,pwを入力ください',
+  }
+  rs.render('users/login',data);
+});
+
+router.post('/add',vCheck(),(rq,rs,next)=>{
+  var request  =rq;
+  var response = rs;
+  var errResult = validationResult(rq);
+  if(!errResult.isEmpty()){
+    var re = '<ul class="error">';
+    var result_arr = errResult.array();
+    for(var n in result_arr){
+        re += '<li>' + result_arr[n].msg + '</li><br>'
+    }
+    re += '</ul>'
+    var data = {title:'Users/Add',
+                content: re,
+                form:rq.body};
+    response.render('users/add',data);
+  }else{
+    request.session.login = null;
+    new User(rq.body).save().then((model)=>{
+      response.redirect('/');
+    });
+  }
+});
+
+router.post('/login',vCheck(),(rq,rs,next)=>{
+  var request  =rq;
+  var response = rs;
+  var errResult = validationResult(rq);
+  if(!errResult.isEmpty()){
+    var re = '<ul class="error">';
+    var result_arr = errResult.array();
+    for(var n in result_arr){
+        re += '<li>' + result_arr[n].msg + '</li><br>'
+    }
+    re += '</ul>'
+    var data = {title:'Users/Add',
+                content: re,
+                form:rq.body};
+    response.render('users/login',data);
+  }else{
+    var nm = rq.body.name;
+    var pw = rq.body.password;
+    User.query({where: {name:nm}, andWhere: {password:pw}}).fetch().then((model)=>{
+      if(model ==null){
+        //ここは冗長
+        var data = {
+          title: '再入力',
+          content: '<p class="error">name or pw が違います</p>',
+          form: rq.body,
+        };
+        response.render('users/login',data);
+      } else {
+        request.session.login = model.attributes;
+        console.log(model.attributes);
+        var data = {
+          title: 'Users/Login',
+          content: '<p>ログインしました!<br>Topに戻ってmsgを送信しましょう</p><br>',
+          form: rq.body
+        };
+        response.render('users/login',data);
+      }
+    }).catch((err)=>{
+      //rs.status(502).json({error: true, data:{message: err.message}});
+      var data = {
+        title: '再入力',
+        content: '<p class="error">name or pw が違います</p>',
+        form: rq.body,
+      };
+      response.render('users/login',data);
+    });
+  }
 });
 
 module.exports = router;
